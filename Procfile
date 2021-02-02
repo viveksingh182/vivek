a57ef809490e105ab9d7382b9f27a702d80d80eb
HSR.Vivek Singh(SMS) <vivek.singh@jslhsr.com>
2021-02-02 13:22:41 +0530

    first commit

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+web: gunicorn gettingstarted.wsgi
 web: gunicorn api:app
 $env:FLASK_APP = "api.py"
--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import flask
 from flask import request, jsonify
 import sqlite3
 
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = False
 
